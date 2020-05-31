f1408dc2f816689cea30bebd2d744e81ddac3a19
Leixb <abone9999@gmail.com>
2020-05-31 17:51:32 +0200

    Util to test

---
+++ parse.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    vector<vector<pair<int, int>>> v(16, vector<pair<int, int>>());
+    vector<int> mx(100, 0);
+
+    string s;
+    cin >> s;
+
+    int n;
+    char c;
+
+    while (cin >> n >> c) {
+        cin >> s;
+        int ej, dia, niv;
+        cin >> c >> ej >> c >> niv >> c >> niv;
+        cin >> c >> dia >> c >> dia;
+        cin >> s;
+        v[dia].push_back({ej, niv});
+    }
+
+    for (int i = 1; i <= 15; ++i) {
+        cout << "# DIA " << i << endl;
+        for (auto p : v[i]) {
+            cout << "    - E" << p.first << "\tN" << p.second << endl;
+            if (p.second < mx[p.first]) {
+                cout << "ERROR ej" << p.first << ' ' << mx[p.first]
+                    << " -> "
+                    << p.second << endl;
+            }
+            mx[p.first] = p.second;
+        }
+    }
+
+    cout << "ASSOLITS" << endl;
+
+    for (int i = 0; i < mx.size(); ++i) {
+        if (mx[i]) cout << "ej " << i << "\tN" << mx[i] << endl;
+    }
+
+    return 0;
+}
