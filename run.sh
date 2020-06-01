4cabed71dc43a74ad17c42e109486fdaeb078d86
Leixb <abone9999@gmail.com>
2020-06-01 19:44:57 +0200

    RUN for diferent versions

--- run.sh
+++ run.sh
@@ -11,14 +11,27 @@ echo " * output  -> $tmp1"
 
 ff="ff-metric"
 
-if [ $# -eq 0 ]; then
+if [ $# -eq 1 ]; then
     echo Please input problem data
     python3 "$input_script" | tee "$problem_file"
+elif [ $# -eq 2 ]; then
+    python3 "$input_script" >"$problem_file" <"$2"
 else
-    python3 "$input_script" >"$problem_file" <"$1"
+    echo "provide more parameters"
+    exit 1
 fi
+
+cd "$1" || {
+    echo "$1 - folder not found"
+    exit 1
+}
+
+echo compiling parser
+
+g++ parser.cpp -o parser
 
 echo running $ff ...
 
-$ff -o basico.pddl -f "$problem_file" | tee "$tmp1"
-grep REALIZAR-EJERCICIO "$tmp1" | cut -c 12- | ./parse
+$ff -o dominio.pddl -f "$problem_file" | tee "$tmp1"
+
+grep REALIZAR-EJERCICIO "$tmp1" | cut -c 12- | ./parser
